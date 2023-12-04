b79b8b1a13c138590981fe2f22e3dbcddf4d3c23
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-12-04 10:01:32 +0000

    clean up and create pydantic schemas

--- server/app/main.py
+++ server/app/main.py
@@ -44,33 +44,15 @@ def root():
     return {"message": "welcome to my python api"}
 
 
-@app.get("/sql")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-
-    return {"data": posts}
-
-
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts""")
-    # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
 
     return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-# Extract all the fields from body stored as a python dict and store it in post
-def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
-    # cursor.execute(
-    #     """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-    #     (post.title, post.content, post.published),
-    # )
-    # new_post = cursor.fetchone()
-
-    # conn.commit()
-
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # ** un packs the dict like ... in javascript, maybe
     new_post = models.Post(**post.dict())
 
@@ -82,8 +64,6 @@ def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
 
 @app.get("/posts/{id}")
 def get_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s  """, (str(id),))
-    # post = cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(
@@ -95,10 +75,6 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-
     post = db.query(models.Post).filter(models.Post.id == id)
 
     if post.first() == None:
@@ -114,15 +90,9 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, updated_post: schemas.Post, db: Session = Depends(get_db)):
-    # cursor.execute(
-    #     """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-    #     (post.title, post.content, post.published, (str(id))),
-    # )
-
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-
+def update_post(
+    id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)
+):
     post_query = db.query(models.Post).filter(models.Post.id == id)
 
     post = post_query.first()
--- server/app/schemas.py
+++ server/app/schemas.py
@@ -1,8 +1,17 @@
 from pydantic import BaseModel
 
 
-# like typescript giving it a type
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
